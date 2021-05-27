5d0ab1e005a36717433f60443d93b7c427e125cd
Allen Lin <allen4353968@gmail.com>
2021-05-27 15:21:39 +0800

    Add test array

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -12,7 +12,7 @@ func walk(x interface{}, fn func(input string)) {
 	case reflect.Struct:
 		numOfValues = val.NumField()
 		getField = val.Field
-	case reflect.Slice:
+	case reflect.Slice, reflect.Array:
 		numOfValues = val.Len()
 		getField = val.Index
 	case reflect.String:
